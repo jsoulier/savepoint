02e7dca4b73c904e59befd055b82c0eee9e21348
Jaan Soulier <jaansoulier@gmail.com>
2026-08-19 00:02:07 -0400

    fix missing mutex on DefaultLogFunction

--- src/log.cpp
+++ src/log.cpp
@@ -1,10 +1,14 @@
 #include <savepoint/log.hpp>
 
 #include <cstdio>
+#include <mutex>
 #include <string_view>
+
+static std::mutex mutex;
 
 static void DefaultLogFunction(std::string_view string)
 {
+    std::scoped_lock lock(mutex);
     std::fwrite(string.data(), sizeof(char), string.size(), stderr);
     std::fputc('\n', stderr);
 }
